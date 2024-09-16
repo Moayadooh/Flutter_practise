f4756bb33e58947915275f18937cde7d50f6dd3f
Moayadooh <moayadsalim97@gmail.com>
2024-09-16 07:48:23 +0400

    route navigation

--- navigation_and_routing/lib/main.dart
+++ navigation_and_routing/lib/main.dart
@@ -1,30 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:navigation_and_routing/product.dart';
+import 'package:navigation_and_routing/first_route.dart';
+import 'package:navigation_and_routing/tabs.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            bottom: const TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.directions_car)),
-                Tab(icon: Icon(Icons.directions_transit)),
-                Tab(icon: Icon(Icons.directions_bike)),
-              ],
-            ),
-          ),
-          body: const TabBarView(
-            children: [
-              Icon(Icons.directions_car),
-              Product(),
-              Icon(Icons.directions_bike),
-            ],
-          ),
-        ),
-      ),
+    const MaterialApp(
+      home: FirstRoute(),
     ),
   );
 }
