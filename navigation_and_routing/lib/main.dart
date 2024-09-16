fa39b5a90193ccf54d590e92e8d228aa7a806f07
Moayadooh <moayadsalim97@gmail.com>
2024-09-16 15:46:39 +0400

    pass data between screens

--- navigation_and_routing/lib/main.dart
+++ navigation_and_routing/lib/main.dart
@@ -1,11 +1,24 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:navigation_and_routing/first_route.dart';
+import 'package:navigation_and_routing/navigation_and_route.dart';
+import 'package:navigation_and_routing/pass_data.dart';
 import 'package:navigation_and_routing/tabs.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: FirstRoute(),
+    MaterialApp(
+      home: TodosScreen(
+        todos: List.generate(
+          20,
+          (i) => Todo(
+            'Todo $i',
+            'A description of what needs to be done for Todo $i',
+          ),
+        ),
+      ),
     ),
+    /*const CupertinoApp(
+      home: CupertinoFirstRoute(),
+    ),*/
   );
 }
---
+++ navigation_and_routing/lib/navigation_and_route.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class FirstRoute extends StatelessWidget {
+  const FirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('First Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            // Navigate to second route when tapped.
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
+
+class CupertinoFirstRoute extends StatelessWidget {
+  const CupertinoFirstRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('First Route'),
+      ),
+      child: Center(
+        child: CupertinoButton(
+          child: const Text('Open route'),
+          onPressed: () {
+            // Navigate to second route when tapped.
+            Navigator.push(
+              context,
+              CupertinoPageRoute(
+                  builder: (context) => const CupertinoSecondRoute()),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class CupertinoSecondRoute extends StatelessWidget {
+  const CupertinoSecondRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Second Route'),
+      ),
+      child: Center(
+        child: CupertinoButton(
+          onPressed: () {
+            // Navigate back to first route when tapped.
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
+        ),
+      ),
+    );
+  }
+}
